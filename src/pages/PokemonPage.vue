64da5abd3c76bb9523d3ed6056e168b3912d6e93
Byron Gonzalez <byrondein@gmail.com>
2023-11-09 00:05:56 -0300

    showPokemon and pokemonId props

---
+++ src/pages/PokemonPage.vue
@@ -0,0 +1,19 @@
+<template>
+    <div>
+        <h1>¿Quién es este pokémon ?</h1>
+        <PokemonPicture v-bind:pokemonId="4" :showPokemon="true"/>
+        <PokemonOptions />  
+    </div>
+  
+</template>
+
+<script>
+import PokemonPicture from '../components/PokemonPicture.vue';
+import PokemonOptions from '../components/PokemonOption.vue';
+export default {
+    components: {
+        PokemonPicture,
+        PokemonOptions
+    },
+}
+</script>
