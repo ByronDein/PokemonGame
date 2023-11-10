b28422d85a6ac32f0518d1ff0aa4bf2f234fa514
Byron Gonzalez <byrondein@gmail.com>
2023-11-10 19:57:26 -0300

    avances app

---
+++ src/api/pokemonApi.js
@@ -0,0 +1,9 @@
+import Axios from "axios";
+
+
+const pokemonApi= Axios.create({
+    baseURL: 'https://pokeapi.co/api/v2/pokemon',
+})
+
+
+export default pokemonApi
--- src/pages/PokemonPage.vue
+++ src/pages/PokemonPage.vue
@@ -2,7 +2,7 @@
     <div>
         <h1>¿Quién es este pokémon ?</h1>
         <PokemonPicture v-bind:pokemonId="4" :showPokemon="true"/>
-        <PokemonOptions />  
+        <PokemonOptions :pokemons="pokemonArr"/>  
     </div>
   
 </template>
@@ -10,10 +10,28 @@
 <script>
 import PokemonPicture from '../components/PokemonPicture.vue';
 import PokemonOptions from '../components/PokemonOption.vue';
+
+import getPokemonOptions from '@/helpers/getPokemonOptions';
+// getPokemonOptions();
 export default {
     components: {
         PokemonPicture,
         PokemonOptions
     },
+    data() {
+        return {
+            pokemonArr:[],
+
+        }
+    },
+    methods: {
+        async mixPoekmonArray() {
+            this.pokemonArr = await getPokemonOptions()
+            console.log(this.pokemonArr);
+        }
+    },
+    mounted() {
+        this.mixPoekmonArray();
+    }
 }
 </script>
