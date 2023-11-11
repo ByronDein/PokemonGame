81f9d0b6457e1e17343b3d050057d14dcb1fe77b
Byron Gonzalez <byrondein@gmail.com>
2023-11-11 01:19:56 -0300

    new game and score player

--- src/pages/PokemonPage.vue
+++ src/pages/PokemonPage.vue
@@ -4,7 +4,15 @@
         <div>
             <h1>¿Quién es este pokémon ?</h1>
             <PokemonPicture v-bind:pokemonId="pokemon.id" :showPokemon="showPokemon" />
-            <PokemonOptions :pokemons="pokemonArr" />
+            <PokemonOptions :pokemons="pokemonArr" @selection="checkAnswer(1, $event)" />
+
+            <div v-if="showAnswer">
+                <h2>{{ message }}</h2>
+                <h2>Score: {{ playerScore }}</h2>
+                <button @click="newGame">
+                    Nuevo Juego
+                </button>
+            </div>
         </div>
     </div>
 </template>
@@ -25,6 +33,9 @@ export default {
             pokemonArr: [],
             pokemon: null,
             showPokemon: false,
+            playerScore: 0,
+            showAnswer: false,
+            message: '',
 
         }
     },
@@ -33,8 +44,30 @@ export default {
             this.pokemonArr = await getPokemonOptions()
             const rndint = Math.floor(Math.random() * (this.pokemonArr.length));
             this.pokemon = this.pokemonArr[rndint];
-        }
+        },
+        checkAnswer(numero, selectedId) {
+            this.showPokemon = true;
+            this.showAnswer = true;
+
+            if (this.pokemon.id === selectedId) {
+                this.playerScore = + 1;
+                this.message = `Correcto ${this.pokemon.name}`;
+            }
+            else {
+                this.playerScore = this.playerScore;
+                this.message = `Oops, era  ${this.pokemon.name}`;
+            }
+        },
+        async newGame() {
+            this.showPokemon = false;
+            this.showAnswer = false;
+            this.pokemon = null;
+            this.message = '';
+            this.playerScore = 0;
+            await this.mixPoekmonArray();
     },
+    },
+   
     mounted() {
         this.mixPoekmonArray();
     }
